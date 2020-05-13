92cdba7f889a2ce394cdbf4c0c06a67009ccf1c5
Juan Andres Quiroz <juanan0309@gmail.com>
2020-05-13 12:04:24 -0500

    reto resuelto y ensayado con tests

--- challenge.py
+++ challenge.py
@@ -2,8 +2,10 @@ def make_division_by(n):
     """This closure returns a function that returns the division
        of an x number by n 
     """
-    # You have to code here!
-    pass
+    def division_n(x):
+        return x/n
+    
+    return division_n
 
 
 def run():
@@ -21,8 +23,20 @@ if __name__ == '__main__':
     import unittest
 
     class ClosureSuite(unittest.TestCase):
-        def test_closure_make_division_by(self):
-            # Make the closure test here
-            pass
+        def setUp(self):
+            self.funcs={3:make_division_by(3),
+                        5:make_division_by(5),
+                        18:make_division_by(18)}
 
+        def test_closure_make_division_by(self):
+            self.assertEqual(5, self.funcs[3](15))
+
+            self.assertEqual(8, self.funcs[5](40))
+
+            self.assertEqual(9, self.funcs[18](162))
+        
+        def tearDown(self):
+            del(self.funcs)
+
+    #unittest.main()
     run()
